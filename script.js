bd3d4971b9bfac318123c6e5e308033c3961cc3d
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-10-05 22:06:22 +0700

    fix js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-const beratBadan = Number(document.getElementById("berat-badan").value)
-const tinggiBadan = Number(document.getElementById("tinggi-badan").value)
+const btnHitung = document.getElementById("btn-hitung");
 
-function hitungBMI () {
-    const hasilBMI = beratBadan/Math.pow(tinggiBadan/100, 2)
-    console.log(hasilBMI)
+btnHitung.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    let kategori
+  btnHitung.onclick = hitungBMI();
 
-    if(hasilBMI >= 0 && hasilBMI < 18.5){
-        kategori = "underweight"
-    }
+function hitungBMI() {
+  let beratBadan = parseFloat(document.getElementById("berat-badan").value);
+  let tinggiBadan = parseFloat(document.getElementById("tinggi-badan").value);
 
-    else if (hasilBMI >= 18.5 && hasilBMI <= 24.9){
-        kategori = "Normal weight"
-    }
-    else if (hasilBMI >= 25 && hasilBMI <= 29.9){
-        kategori = "Overweight"
-    }
-    else if (hasilBMI >= 30){
-        kategori = "Obesity"
-    }
-    else {
-        kategori = "The result is invalid"
-    }
-    
-    const outputBMI = document.getElementById("output")
-    outputBMI.innerHTML = `Your BMI is ${hasilBMI}. Which means you are ${kategori}`
+  let hasilBMI = beratBadan / Math.pow(tinggiBadan / 100, 2);
+  console.log(hasilBMI);
+
+  let kategori;
+
+  if (hasilBMI >= 0 && hasilBMI < 18.5) {
+    kategori = "underweight";
+  } else if (hasilBMI >= 18.5 && hasilBMI <= 24.9) {
+    kategori = "Normal weight";
+  } else if (hasilBMI >= 25 && hasilBMI <= 29.9) {
+    kategori = "Overweight";
+  } else if (hasilBMI >= 30) {
+    kategori = "Obesity";
+  } else {
+    kategori = "This score is invalid";
+  }
+
+  const outputBMI = document.getElementById("output");
+  outputBMI.innerHTML = `Your BMI is ${hasilBMI.toFixed(2)} (${kategori})`;
+
+  document.getElementById("berat-badan").value = "";
+  document.getElementById("tinggi-badan").value = "";
 }
-
-
+});
